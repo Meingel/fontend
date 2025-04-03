21fb76a4d251e76f9320fa8bbcebe7eb45b13c3b
Meingel <92056329+Meingel@users.noreply.github.com>
2025-04-03 14:59:06 -0500

    Primer commit
    Se agrega menu de cabecera, paginas a trabajar y css básico

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const button = document.querySelector('.hero button');
+    button.addEventListener('click', () => {
+        alert('Bienvenido a nuestra tienda virtual UrbanStyle.');
+    });
+});
